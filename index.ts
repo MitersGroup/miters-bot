53bcdcfc3c1053a7f3eeadb1c432165ddbd14281
yyueniao <yyueniao0913@gmail.com>
2023-07-13 07:47:49 +0800

    build(init): init project

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import "dotenv/config";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
