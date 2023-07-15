1fcfa7078c24012f6865a76573ba39b06cfc0aeb
yyueniao <91043129+yyueniao@users.noreply.github.com>
2023-07-15 10:56:20 +0800

    build: `prettier` (#3)
    * build(prettier): add dependency
    
    * style(prettier):o format whole project
    
    This commit add custom script `format` and `format-fix` as well.
    
    * fix(eslint): extend `prettier` config
    
    This config is to avoid conflicts between eslint and prettier.
    
    * build(ci): check format
    
    Also update `actions/checkout` version in this commit.

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 import "dotenv/config";
-import {Client, Events, GatewayIntentBits} from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-const client = new Client({"intents": [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(
-    Events.ClientReady,
-    (onReadyClient) => {
-
-        console.log(`Ready! Logged in as ${onReadyClient.user.tag}`);
-
-    }
-);
+client.once(Events.ClientReady, (onReadyClient) => {
+  console.log(`Ready! Logged in as ${onReadyClient.user.tag}`);
+});
 
 void client.login(process.env.DISCORD_TOKEN);
