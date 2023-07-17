63ef7fab96df209d732a2756d6bcb8250b4f2c4f
yyueniao <91043129+yyueniao@users.noreply.github.com>
2023-07-17 23:00:17 +0800

    refactor(commands/ping): avoid hardcoding response (#19)

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,9 +1,10 @@
 import { Client, CommandInteraction } from "discord.js";
+import contents from "../contents/ping";
 
 export const ping = {
   name: "ping",
-  description: 'Test command. Will reply with "Pong!".',
+  description: contents.DESCRIPTION,
   async execute(_client: Client, interaction: CommandInteraction) {
-    await interaction.reply("Pong!");
+    await interaction.reply(contents.PONG);
   },
 };
