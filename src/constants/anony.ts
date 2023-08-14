825cff0ba5d92acc250356827076c16195cdd764
Oyster Lee <oysterd3@gmail.com>
2023-08-14 20:56:49 +0800

    refactor: restructure constants

---
+++ src/constants/anony.ts
@@ -0,0 +1,14 @@
+const ANONY_CONSTANTS = {
+  name: 'anony',
+  description: 'Test command. Will submit a message anonymously.',
+  subCommandTitleName: 'title',
+  subCommandTitleDescription: 'Title for the post',
+  subCommandMessageName: 'message',
+  subCommandMessageDescription: 'Message to be sent as anonymously',
+  contentLabel: 'Content',
+  defaultColorCode: 0x0000ff,
+  approvedColorCode: 0x00ff00,
+  rejectedColorCode: 0xff0000,
+} as const;
+
+export default ANONY_CONSTANTS;
--- src/contents/ping.ts
+++
@@ -1,6 +0,0 @@
-const contents = {
-  PONG: "Pong!",
-  DESCRIPTION: 'Test command. Will reply with "Pong!".',
-};
-
-export default contents;
