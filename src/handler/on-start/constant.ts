60725bf5790320a2967f2bb994465867a1f63152
Pwkang <porweikang@hotmail.com>
2023-08-14 23:01:57 +0800

    refactor: remove unnecessary imports
    Co-authored-by: yyueniao <91043129+yyueniao@users.noreply.github.com>

--- src/handler/on-start/constant.ts
+++ src/handler/on-start/constant.ts
@@ -1,7 +1,3 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
 const NODE_ENV = process.env["NODE_ENV"] ?? "development";
 const production = NODE_ENV === "production";
 export const handlerRoot = production ? "dist" : "src";
