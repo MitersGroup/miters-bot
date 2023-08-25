df2b3fbb7d0648f599f5a00c5bc0edfb079dd03f
yyueniao <yyueniao0913@gmail.com>
2023-08-25 20:55:26 +0800

    refactor(eventHandler): remove unused type

--- src/events/events.handler.ts
+++ src/events/events.handler.ts
@@ -1,13 +1,6 @@
-import { Client, ClientEvents } from "discord.js";
+import { Client } from "discord.js";
 import { loadClientReadyEvents } from "./client-ready/clientReadyEvents.handler";
 import { loadInteractionCreateEvents } from "./interaction-create/interactionCreateEvents.handler";
-
-export interface BotEvent {
-  once: boolean;
-  eventName: keyof ClientEvents;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  execute: (client: Client<true>, ...args: any[]) => Promise<void>;
-}
 
 export default async function registerEvents(client: Client) {
   console.log("Loading events...");
